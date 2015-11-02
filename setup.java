b7037e2f1fd8c610e0031ba0e8432f94bb94a2bc
eccentricayman <aahmed3@stuy.edu>
2015-11-01 21:25:15 -0400

    fix the dir making in setup

--- setup.java
+++ setup.java
@@ -5,25 +5,27 @@ import java.io.*;
 public class setup {
     public static void main(String[] args) {
         Scanner userin = new Scanner(System.in);
-        int hwnum;
-        String hwsummary;
-        int period;
-        String name;
+        int hwnum = 0;
+        String hwsummary = "";
+        int period = 0;
+        String name = "";
+        String filename = "";
         System.out.println("Enter name: ");
         name = userin.nextLine();
         System.out.println("Enter APCS period: ");
         period = userin.nextInt();
         System.out.print("Enter HW number: ");
         hwnum = userin.nextInt();
+        new File("/hw" + hwnum + "/").mkdirs();
+        String filler = userin.nextLine();
         System.out.println("Enter HW summary: ");
         hwsummary = userin.nextLine();
         System.out.println("Enter file name: ");
-        String filename = userin.nextLine();
-        filename = filename + ".java";
-        new File("/hw" + hwnum + "/").mkdirs();
+        filename = userin.nextLine();
+        filename += ".java";
         new File("/hw" + hwnum +"/" + filename);
         try {
-    FileWriter outputStream = new FileWriter(filename);
+    FileWriter outputStream = new FileWriter("/hw" + hwnum +"/" + filename);
     try (BufferedWriter out = new BufferedWriter(outputStream)) {
         out.write("/*");
         out.newLine();
@@ -31,7 +33,7 @@ public class setup {
         out.newLine();
         out.write("APCS1 pd" + period);
         out.newLine();
-        out.write("HW" + hwnum + " -- " + hwsummary);
+        out.write("HW" + "#" + hwnum + " -- " + hwsummary);
         out.newLine();
         out.write(getdate());
         out.newLine();
