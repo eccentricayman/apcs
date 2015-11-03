6b073e7c0995c5cd8b10a94dac1c50fb8b55519c
eccentricayman <aahmed3@stuy.edu>
2015-11-03 14:38:23 -0400

    cleaned class files

--- hw22/ArrayUtils.java
+++ hw22/ArrayUtils.java
@@ -5,25 +5,24 @@ HW#22 -- Augmenting Your Array of Tools
 2015-11-02
 */
 
-import java.util.Random;
-
 public class ArrayUtils {
-	Random rnum = new Random();
-    public static void Populate(int[] a) {
-	for (int i = 0 ; i < a.length ; i++) {
-	    a[i] = rnum.nextInt(Integer.MAX_VALUE);
-	}
+    public static void Populate(int[] a, int range) {
+		for (int i = 0 ; i < a.length ; i++) {
+	    	a[i] = (int)(Math.random()*range + 1);    //takes an array and a range as input, fills array with ints from 0 to range
+		}
     }
     public static String arrayReturn(int[] a) {
-	String retstr = "";
-	for (int i = 0 ; i < a.length ; i++) {
-	    retstr += Integer.toString(a[i]);
-	}
-	return retstr;
+		String retstr = "";
+		for (int i = 0 ; i < a.length - 1; i++) {
+	    	retstr += Integer.toString(a[i]);   
+	    	retstr += ", ";    // seperates values by a comma
+		}
+		retstr += a[a.length - 1];   //adds last value to the string
+		return retstr;
     }
     public static void main(String[] args) {
-	int[] array =  new int[5];
-	Populate(array);
-	System.out.println(arrayReturn(array));
+		int[] array =  new int[5];
+		Populate(array, 15);     //testing methods
+		System.out.println(arrayReturn(array));
     }
 }
