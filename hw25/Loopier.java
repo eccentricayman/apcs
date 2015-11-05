e3712fd4487fd7c7d38122152020b9f8630d1bac
eccentricayman <aahmed3@stuy.edu>
2015-11-05 16:31:40 -0500

    loopier

---
+++ hw25/Loopier.java
@@ -0,0 +1,10 @@
+/*
+Ayman Ahmed
+APCS1 pd5
+HW#25 -- Recurse, Recurse, on Elements Not the First! 
+2015-11-05
+*/
+
+public class Loopier {
+    public static int linSearch(int[] a, int target) {
+	
