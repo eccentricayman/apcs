9cd6fdd3fd1f6cae50c96be29f170d57f6cc545c
eccentricayman <aahmed3@stuy.edu>
2015-11-23 11:37:33 -0500

    fixed tabbing

--- hw36/Concentration.java
+++ hw36/Concentration.java
@@ -1,24 +1,29 @@
-import cs1.Keyboard;  //if comfortable with Scanner, you may comment this out
+/import cs1.Keyboard;  //if comfortable with Scanner, you may comment this out
 
 		
 public class Concentration {
-
-		//instance variables
-		private Tile[][] _board;     //storage for 4x4 grid of Tiles.
-		private int _numberFaceUp;   //count of Tiles with faces visible
-		private String[] _words;     //list of unique Strings used for Tile vals
-		private static int _numRows = 4;
-
-
-		//insert constructor and methods here
-
-
-		
+    
+    //instance variables
+    private Tile[][] _board;     //storage for 4x4 grid of Tiles.
+    private int _numberFaceUp;   //count of Tiles with faces visible
+    private String[] _words;     //list of unique Strings used for Tile vals
+    private static int _numRows = 4;
+    
+    //insert constructor and methods here
+    public Concentration() {
+	_board = new Tile[_numRows][_numRows];
+	_numberFaceUp = 0;
+	for (int i = 0 ; i < _numRows ; i++) {
+	    _words[i] = 
+	}
+    }
+    
+    
 		//DO NOT MODIFY main()
-		public static void main(String[] args){
-				Concentration game = new Concentration();
-				game.play();
-		}
-
+    public static void main(String[] args) {
+	Concentration game = new Concentration();
+	game.play();
+    }
+    
 }//end class Concentration
 
