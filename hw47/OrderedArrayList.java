1b0028ad998d190f33f152356489ca5946bff180
eccentricayman <aahmed3@stuy.edu>
2015-12-11 11:37:30 -0500

    needed addbinary

--- hw47/OrderedArrayList.java
+++ hw47/OrderedArrayList.java
@@ -1,8 +1,8 @@
 /*
   Ayman Ahmed
   APCS1 pd5
-  HW#46 -- ascending
-  2015-12-10
+  HW#47 --
+  2015-12-11
 */
 
 /*============================================
@@ -18,72 +18,78 @@ import java.util.ArrayList;
 
 public class OrderedArrayList {
 
-	// instance of class ArrayList, holding objects of type Comparable 
-	// (ie, instances of a class that implements interface Comparable)
-	private ArrayList<Comparable> _data;
+    // instance of class ArrayList, holding objects of type Comparable 
+    // (ie, instances of a class that implements interface Comparable)
+    private ArrayList<Comparable> _data;
 
 
-	// default constructor initializes instance variable _data
-	public OrderedArrayList() {
+    // default constructor initializes instance variable _data
+    public OrderedArrayList() {
 	_data = new ArrayList<Comparable>();
-	}
+    }
 
 
-	public String toString() { 
+    public String toString() { 
 	return _data.toString();
-	}
+    }
 
 
-	public Comparable rempove( int index ) { 
-		Comparable temp = _data.get(index);
-		_data.remove(index);
-		return temp;
-	}
+    public Comparable rempove( int index ) { 
+	Comparable temp = _data.get(index);
+	_data.remove(index);
+	return temp;
+    }
 
 
-	public int size() { 
-		return _data.size();
-	}
+    public int size() { 
+	return _data.size();
+    }
 
 	
-	public Comparable get( int index ) { 
-		return _data.get(index);
+    public Comparable get( int index ) { 
+	return _data.get(index);
+    }
+
+
+    // addLinear takes as input any comparable object 
+    // (i.e., any object of a class implementing interface Comparable)
+    // inserts newVal at the appropriate index
+    // maintains ascending order of elements
+    // uses a linear search to find appropriate index
+    public void addLinear( Comparable newVal ) { 
+	if (_data.size() == 0) {
+	    _data.add(newVal);
+	}
+	else {
+	    for (int i = 0 ; i < _data.size() ; i++) {
+		_data.add(i, newVal);
+	    }
+	}
+    }
+    
+    public void addBinary(Comparable newVal) {
+	int lo = 0;
+	int med = 0;
+	int hi = _data.size() - 1;
+	//todo
+    }
+    public static void main( String[] args ) {
+
+	OrderedArrayList Franz = new OrderedArrayList();
+
+	System.out.println("\nValues to add via addLinear() calls:");
+
+	// testing linear search
+	for( int i = 0; i < 15; i++ ) {
+	    int valToAdd = (int)( 50 * Math.random() );
+	    System.out.println( valToAdd );
+	    Franz.addLinear( valToAdd );
 	}
 
+	System.out.println("\nafter population via addLinear() calls:");
+	System.out.println( Franz );
+	System.out.println();
 
-	// addLinear takes as input any comparable object 
-	// (i.e., any object of a class implementing interface Comparable)
-	// inserts newVal at the appropriate index
-	// maintains ascending order of elements
-	// uses a linear search to find appropriate index
-	public void addLinear( Comparable newVal ) { 
-		if (_data.size() == 0) {
-			_data.add(newVal);
-		}
-		else {
-			for (int i = 0 ; i < _data.size() ; i++) {
-			_data.add(i, newVal);
-			}
-		}
-	}
-
-	public static void main( String[] args ) {
-
-		OrderedArrayList Franz = new OrderedArrayList();
-
-		System.out.println("\nValues to add via addLinear() calls:");
-
-		// testing linear search
-		for( int i = 0; i < 15; i++ ) {
-			int valToAdd = (int)( 50 * Math.random() );
-			System.out.println( valToAdd );
-			Franz.addLinear( valToAdd );
-		}
-
-		System.out.println("\nafter population via addLinear() calls:");
-		System.out.println( Franz );
-		System.out.println();
-
-	}
+    }
 
 }//end class OrderedArrayList
