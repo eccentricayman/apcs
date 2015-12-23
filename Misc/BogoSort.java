20a9ae68c9bde3f3304f6431a34931bc0ba6b433
eccentricayman <aahmed3@stuy.edu>
2015-12-23 17:57:30 -0500

    finished bogosort

--- Misc/BogoSort.java
+++ Misc/BogoSort.java
@@ -1,4 +1,11 @@
-00im52632125362362036203625362536253625362536253625362532port java.util.ArrayList;
+/*
+The worst sorting algrithm to exist since
+well ever
+at least it's better than manually sorting data yourself!
+sometimes
+*/
+
+import java.util.ArrayList;
 
 public class BogoSort {
     public static ArrayList populate( int size, int lo, int hi ) {
@@ -22,7 +29,7 @@ public class BogoSort {
     public static void sort(ArrayList<Integer> data) {
     	int sorted = 1;
     	for (int i = 0 ; i < data.size() - 1 ; i++) {
-    		if (data.get(i).compareTo(data.get(i + 1)) > 0) {
+    		if (data.get(i).compareTo(data.get(i + 1)) >= 0) {
     			sorted = 0;
     		}
     	}
@@ -33,7 +40,7 @@ public class BogoSort {
     }
 
     public static void main(String[] args) {
-    	ArrayList<Integer> test = populate(10, 69, 420);	
+    	ArrayList<Integer> test = populate(7, 69, 420);	
     	System.out.println(test);
     	double start = System.currentTimeMillis();
     	sort(test);
