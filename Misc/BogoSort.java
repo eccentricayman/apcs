2424f403709418fcd464714b28404664f1ef1ab9
eccentricayman <aahmed3@stuy.edu>
2015-12-23 15:11:41 -0500

    bleh

--- Misc/BogoSort.java
+++ Misc/BogoSort.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+00im52632125362362036203625362536253625362536253625362532port java.util.ArrayList;
 
 public class BogoSort {
     public static ArrayList populate( int size, int lo, int hi ) {
@@ -22,7 +22,7 @@ public class BogoSort {
     public static void sort(ArrayList<Integer> data) {
     	int sorted = 1;
     	for (int i = 0 ; i < data.size() - 1 ; i++) {
-    		if (data.get(i).compareTo(data.get(i + 1)) < 0) {
+    		if (data.get(i).compareTo(data.get(i + 1)) > 0) {
     			sorted = 0;
     		}
     	}
@@ -33,7 +33,12 @@ public class BogoSort {
     }
 
     public static void main(String[] args) {
-    	ArrayList<Integer> test = new ArrayList<Integer>();
-    	
+    	ArrayList<Integer> test = populate(10, 69, 420);	
+    	System.out.println(test);
+    	double start = System.currentTimeMillis();
+    	sort(test);
+    	double end = System.currentTimeMillis();
+    	System.out.println(end - start);
+    	System.out.println(test);
     }
 }
