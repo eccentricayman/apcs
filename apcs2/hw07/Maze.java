825187754b353f4def204e6f0c4f57208490df3a
eccentricayman <aahmed3@stuy.edu>
2016-03-01 11:26:45 -0500

    added maze dat

--- apcs2/hw07/Maze.java
+++ apcs2/hw07/Maze.java
@@ -1,8 +1,8 @@
 /*
-Ayman Ahmed
-APCS2 pd5
-HW#7 -- Amaizing Execution
-2016-03-01
+  Ayman Ahmed
+  APCS2 pd5
+  HW#7 -- Amaizing Execution
+  2016-03-01
 */
 
 /*======================================
@@ -33,69 +33,69 @@ class MazeSolver {
 
     public MazeSolver( String inputFile ) {
 
-	//init 2D array to represent maze 
-	// ...same dimensions as default terminal window
-	maze = new char[80][25];
-	h = 0;
-	w = 0;
+        //init 2D array to represent maze 
+        // ...same dimensions as default terminal window
+        maze = new char[80][25];
+        h = 0;
+        w = 0;
 
-	try {
-	    Scanner sc = new Scanner( new File(inputFile) );
+        try {
+            Scanner sc = new Scanner( new File(inputFile) );
 
-	    System.out.println( "reading in file..." );
+            System.out.println( "reading in file..." );
 
-	    int row = 0;
+            int row = 0;
 
-	    while( sc.hasNext() ) {
+            while( sc.hasNext() ) {
 
-		String line = sc.nextLine();
+                String line = sc.nextLine();
 
-		if ( w < line.length() ) 
-		    w = line.length();
+                if ( w < line.length() ) 
+                    w = line.length();
 
-		for( int i=0; i<line.length(); i++ )
-		    maze[i][row] = line.charAt( i );
+                for( int i=0; i<line.length(); i++ )
+                    maze[i][row] = line.charAt( i );
 
-		h++;
-		row++;
-	    } 
+                h++;
+                row++;
+            } 
 
-	    for( int i=0; i<w; i++ )
-		maze[i][row] = WALL;
-	    h++;
-	    row++;
+            for( int i=0; i<w; i++ )
+                maze[i][row] = WALL;
+            h++;
+            row++;
 
-	} catch( Exception e ) { System.out.println( "Error reading file" ); }
+        } catch( Exception e ) { System.out.println( "Error reading file" ); }
 
 
-	solved = false;
+        solved = false;
     }//end constructor
 
 
     public String toString() {
-	//send ANSI code "ESC[0;0H" to place cursor in upper left
-	String retStr = "[0;0H";  
-	//emacs shortcut: C-q, then press ESC
-	//emacs shortcut: M-x quoted-insert, then press ESC
+        //send ANSI code "ESC[0;0H" to place cursor in upper left
+        String retStr = "[0;0H";  
+        //emacs shortcut: C-q, then press ESC
+        //emacs shortcut: M-x quoted-insert, then press ESC
 
-	int i, j;
-	for( i=0; i<h; i++ ) {
-	    for( j=0; j<w; j++ )
-		retStr = retStr + maze[j][i];
-	    retStr = retStr + "\n";
-	}
-	return retStr;
+        int i, j;
+        for( i=0; i<h; i++ ) {
+            for( j=0; j<w; j++ )
+                retStr = retStr + maze[j][i];
+            retStr = retStr + "\n";
+        }
+        return retStr;
     }
 
 
     //helper method to keep try/catch clutter out of main flow
     private void delay( int n ) {
-	try {
-	    Thread.sleep(n);
-	}
-	catch( InterruptedException e ) {
-	    System.exit(0);
-	}
+        try {
+            Thread.sleep(n);
+        }
+        catch( InterruptedException e ) {
+            System.exit(0);
+        }
     }
 
 
@@ -106,21 +106,21 @@ class MazeSolver {
      *********************************************/
     public void solve( int x, int y ) {
 
-	delay(50); //slow it down enough to be followable
+        delay(50); //slow it down enough to be followable
 
-	//primary base case
-	if (  ) {
-	}
-	//other base case(s)...
-	else if ( ) {
-	}
-	else if ( ) {
-	}
-	else if ( ) {
-	}
-	//recursive reduction
-	else {
-	}
+        //primary base case
+        if (  ) {
+        }
+        //other base case(s)...
+        else if ( ) {
+        }
+        else if ( ) {
+        }
+        else if ( ) {
+        }
+        //recursive reduction
+        else {
+        }
     }
 
     //accessor method to help with randomized drop-in location
@@ -133,38 +133,38 @@ public class Maze {
 
     public static void main( String[] args ) {
 
-	try {
-	    String mazeInputFile = args[0];
+        try {
+            String mazeInputFile = args[0];
 
-	    MazeSolver ms = new MazeSolver( mazeInputFile );
-	    //clear screen
-	    System.out.println( "[2J" ); 
+            MazeSolver ms = new MazeSolver( mazeInputFile );
+            //clear screen
+            System.out.println( "[2J" ); 
 
-	    //display maze 
-	    System.out.println( ms );
+            //display maze 
+            System.out.println( ms );
 
-	    //drop hero into the maze (be sure coords are on the path)
-	    //comment next line out when ready to randomize startpos
-	    ms.solve( 4, 3 ); 
+            //drop hero into the maze (be sure coords are on the path)
+            //comment next line out when ready to randomize startpos
+            ms.solve( 4, 3 ); 
 
-	    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	    //drop our hero into maze at random location on path
-	    //the Tim Diep way:
-	    Random r = new Random();
-	    int startX = r.nextInt( 80 );
-	    int startY = r.nextInt( 25 );
-	    while ( !ms.onPath(startX,startY) ) {
-		startX = r.nextInt( 80 );
-		startY = r.nextInt( 25 );
-	    }
+            /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //drop our hero into maze at random location on path
+            //the Tim Diep way:
+            Random r = new Random();
+            int startX = r.nextInt( 80 );
+            int startY = r.nextInt( 25 );
+            while ( !ms.onPath(startX,startY) ) {
+            startX = r.nextInt( 80 );
+            startY = r.nextInt( 25 );
+            }
 
-	    ms.solve( startX, startY );
-	    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	}
-	catch( Exception e ) { 
-	    System.out.println( "Error reading input file." );
-	    System.out.println( "Usage: java Maze <filename>" ); 
-	}
+            ms.solve( startX, startY );
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        }
+        catch( Exception e ) { 
+            System.out.println( "Error reading input file." );
+            System.out.println( "Usage: java Maze <filename>" ); 
+        }
     }
 
 }//end class Maze
