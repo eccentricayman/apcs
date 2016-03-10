ebbe9fbb6875bda6fbd673ffeebd7a90f053daff
eccentricayman <aahmed3@stuy.edu>
2016-03-10 00:24:37 -0500

    finished timetesting

--- apcs2/hw11/MergeSortTester.java
+++ apcs2/hw11/MergeSortTester.java
@@ -1,8 +1,8 @@
 /*
-Team Winter  -- Ayman Ahmed, Andy Liang, Aleksandar Shipetich
-APCS2 pd5
-HW#11 -- <BLANK>
-2016-03-09
+  Team Winter  -- Ayman Ahmed, Andy Liang, Aleksandar Shipetich
+  APCS2 pd5
+  HW#11 -- <BLANK>
+  2016-03-09
 */
 
 /*======================================
@@ -28,6 +28,8 @@ HW#11 -- <BLANK>
   <INSERT YOUR RESULTS ANALYSIS HERE>
   ======================================*/
 
+import java.util.Scanner;
+
 public class MergeSortTester {
 
     /******************************
@@ -38,20 +40,43 @@ public class MergeSortTester {
      ******************************/
     
     //helper to randomly fill array
-    public static void fillArr(int[] arr) {
+    public static int[] randArr(int n) {
+        int[] arr = new int[n];
         for (int i = 0 ; i < arr.length ; i++) {
-            arr[i] = (Math.random() * 100) + 1
+            arr[i] = (int)(Math.random() * 100) + 1;
         }
+        return arr;
     }
+
+    public static void printArray( int[] a ) {
+        System.out.print("[");
+        for( int i : a )
+            System.out.print( i + ",");
+        System.out.println("]");
+    }
+ 
     
     //returns how long an array takes to mergesort in seconds
-    public static double timeTest(int[] arr, int n) {
-        double start = System.currentTimeMillis();
-        
+    public static void timeTest(int n) {
+        int[] arr = randArr(n);
+        System.out.println("Unsorted Array of length " + n);
+        printArray(arr);
+        long start = System.nanoTime();
+        MergeSort.sort(arr);
+        long end = System.nanoTime();
+        System.out.println("Sorted Array");
+        printArray(arr);
+        System.out.println("Time: " + (end - start) + " nanoseconds");
     }
     
     public static void main( String[] args ) {
-        System.out.println('penis"');
+        //keep testing until user Ctrl + C's
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter an array length to test or Ctrl + C to stop: ");
+            int n = sc.nextInt();
+            timeTest(n);
+        }
     }//end main
     
 }//end class
