4d0eb788bd0cdce5e8fc7a3468a1cc1584a80b39
eccentricayman <aahmed3@stuy.edu>
2016-03-14 11:47:26 -0400

    started llnode and llist

---
+++ apcs2/hw12b/LList.java
@@ -0,0 +1,6 @@
+/*
+Ayman Ahmed
+APCS2 pd5
+HW#12 -- Truckin' Through the Night
+2016-03-14
+*/
---
+++ apcs2/hw12b/List.java
@@ -0,0 +1,8 @@
+public interface List {
+
+    public boolean add(String x);
+    public String get(int i);
+    public String set(int i, String x);
+    public int size();
+
+}
