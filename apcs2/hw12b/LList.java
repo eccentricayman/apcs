d1cc0399af2be63df49a2a3c9a1a81ad1cb479fb
eccentricayman <aahmed3@stuy.edu>
2016-03-14 17:02:14 -0400

    started llist

--- apcs2/hw12b/LList.java
+++ apcs2/hw12b/LList.java
@@ -4,3 +4,24 @@ APCS2 pd5
 HW#12 -- Truckin' Through the Night
 2016-03-14
 */
+
+public class LList implements List {
+
+	private LLNode beginning;
+	private LLNode end;
+	private int nodecount;
+
+	public LList() {
+		beginning = null;
+		end = null;
+	}
+
+	public int size() {
+		return nodecount;
+	}
+
+	public boolean add(String x) {
+
+	}
+	
+}
