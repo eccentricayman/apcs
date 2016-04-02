189d61db3074d91c96f94a664de1e97206f8fa31
eccentricayman <aahmed3@stuy.edu>
2016-04-01 20:24:53 -0400

    dir structure for useful code

---
+++ apcs2/RandomUsefulTidbits/Scheme.java
@@ -0,0 +1,27 @@
+/***************************************
+ * class Scheme
+ * A rudimentary Scheme interpreter
+ *
+ * Algo:
+ * 
+ * 1. Eat some pancakes
+ * 2. Parse with various functions
+ * 3. Each opening parens triggers a function which calls other functions
+ * based on the next operator to appear
+ * 4. Function will return the value, and give it to the function above
+ * 5. Eat your pancakes, this time with satisfaction as you uninstall 
+ * DrRacket
+ *
+ * Stack of choice: ALStack by Ayman Ahmed
+ *
+ * b/c: ALStack doesn't need to traverse the entire list to reach an
+ * index, and speed is maximum priority
+ ***************************************/
+
+public class Scheme {
+    
+    public static int add(int a, int b) {
+        
+    }
+
+}
