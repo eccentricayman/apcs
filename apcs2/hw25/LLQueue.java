455f1d71eb1bc4be6f2f50fa18649d3941e0cdf6
eccentricayman <aahmed3@stuy.edu>
2016-04-04 16:57:50 -0400

    bleh

--- apcs2/hw25/LLQueue.java
+++ apcs2/hw25/LLQueue.java
@@ -1,6 +1,98 @@
 /*
-Ayman Ahmed
+Team NullPointerException -- Ayman Ahmed and Adam McKoy
 APCS2 pd5
 HW#25 -- Brits Do Not Wait in Line
 2016-04-04
 */
+
+/*****************************************************
+ * class LLQueue
+ * uses a linked list to implement a QUEUE
+ * (a collection with FIFO property)
+ *
+ *       -------------------------------
+ *   end |  --->   Q U E U E   --->    | front
+ *       -------------------------------
+ *
+ ******************************************************/
+
+import java.util.NoSuchElementException;
+import java.util.RuntimException;
+
+public class LLQueue<T> implements Queue<T> {
+
+    private LLNode<T> _front, _end;
+
+    // default constructor creates an empty queue
+    public LLQueue() {
+        _end = new LLNode<T>(null, null);
+        _front = new LLNode<T>(null, _end);
+    }
+
+    // means of adding a thing to the collection
+    public void enqueue( T enQVal ) {
+        LLNode<T> temp = new LLNode(enQVal, null);
+        LLNode<T> current = _front;
+        while (!(current.getNext().equals(_end))) {
+            current = current.getNext();
+        }
+        current.setNext(temp);
+        temp.setnext(_end);
+    }//O(n)
+
+    // means of removing a thing from the collection
+    // remove and return thing at front of queue
+    // assume _queue ! empty
+    public T dequeue() {
+        T temp = _front.getNext().getValue();
+        if (_front.getNext().equals(_end)) {
+            throw new NoSuchElementException("WOOHOO EMPTY LINE :DDD");
+        }
+        else {
+            _front.setNext(_front.getNext().getNext());
+        }
+    }//O(1)
+
+    // means of peeking at thing next in line for removal
+    public T peekFront() {
+        return _front.getNext().getValue();
+    }//O(1)
+
+    public boolean isEmpty() {
+        return _front.getNext().equals(_end);
+    }//O(1)
+
+    // print each node, separated by spaces
+    public String toString() {
+        String retstr = "NULL ";
+        current = _front.getNext();
+        while (!(current.getNext().equals(_end))) {
+            retstr += "-> " + current.getValue() + " ";
+        }
+    }//O(?)
+
+    public static void main( String[] args ) {
+
+	/*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+	Queue<String> LLQueuelJ = new LLQueue<String>();
+
+	System.out.println("\nnow enqueuing thrice...");
+	LLQueuelJ.enqueue("James");
+	LLQueuelJ.enqueue("Todd");
+	LLQueuelJ.enqueue("Smith");
+
+	System.out.println("\nnow testing toString()...");
+	System.out.println( LLQueuelJ ); //for testing toString()...
+
+	System.out.println("\nnow dequeuing thrice...");
+	System.out.println( LLQueuelJ.dequeue() );
+	System.out.println( LLQueuelJ.dequeue() );
+	System.out.println( LLQueuelJ.dequeue() );
+
+	System.out.println("\nDequeuing from empty queue should yield error...");
+	System.out.println( LLQueuelJ.dequeue() );
+	  ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+
+    }//end main
+
+}//end class LLQueue
--- apcs2/hw26/ALQueue.java
+++ apcs2/hw26/ALQueue.java
@@ -1,6 +1,85 @@
 /*
-Ayman Ahmed
+Team NullPointerException -- Ayman Ahmed and Adam McKoy
 APCS2 pd5
 HW#26 -- Nor Do Aussies
 2016-04-04
 */
+
+/*****************************************************
+ * class ALQueue
+ * uses an ArrayList to implement abstract data type QUEUE
+ * (a collection with FIFO property)
+ *
+ *       -------------------------------
+ *   end |  --->   Q U E U E   --->    | front
+ *       -------------------------------
+ *
+ ******************************************************/
+
+import java.util.ArrayList;
+
+
+public class ALQueue<T> implements Queue<T> {
+
+    private ArrayList<T> _queue;
+
+    // default constructor
+    public ALQueue() {
+
+    }
+
+    // means of adding an item to collection
+    public void enqueue( T x ){
+
+    } //O()
+
+
+
+    // means of removing an item from collection
+    public T dequeue() {
+
+    } //O()
+
+
+
+    // means of "peeking" at the front item
+    public T peekFront() {
+
+    } //O()
+
+    // means of checking to see if collection is empty
+    public boolean isEmpty() {
+
+    } //O()
+
+    public String toString() {
+
+    }
+
+    //main method for testing
+    public static void main( String[] args ) {
+
+	/*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+	Queue<String> ALBSure = new ALQueue<String>();
+
+	System.out.println("\nnow enqueuing thrice...");
+	ALBSure.enqueue("Al");
+	ALBSure.enqueue("B.");
+	ALBSure.enqueue("Sure!");
+
+	System.out.println("\nnow testing toString()...");
+	System.out.println( ALBSure ); //for testing toString()...
+
+	System.out.println("\nnow dequeuing thrice...");
+	System.out.println( ALBSure.dequeue() );
+	System.out.println( ALBSure.dequeue() );
+	System.out.println( ALBSure.dequeue() );
+
+	System.out.println("\nDequeuing from empty queue should yield error...");
+	System.out.println( ALBSure.dequeue() );
+	  ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+
+    }//end main
+
+
+}//end class ALQueue
