df98067fb6b015a2b24feb82639a219ba099fd0f
eccentricayman <aahmed3@stuy.edu>
2016-05-11 16:21:58 -0400

    finished arraybst

--- apcs2/hw37/ArrayBST.java
+++ apcs2/hw37/ArrayBST.java
@@ -27,32 +27,117 @@ public class ArrayBST {
     }
 
     public void insert(int newVal) {
-        if (!isFull(tree)) {
-            int root = 0;
-            while (root != -1) {
-                if (tree[root] == -1) {
-                    tree[root] = newVal;
-                    root = -1;
-                }
-                else if (newVal >= tree[root]) {
-                    root *= 2;
-                    root++;
-                }
-                else if (newVal < tree[root]) {
-                    root *= 2;
+        int index = 0;
+        boolean added = false;
+        while (!added) {
+            if (tree[index] == -1) {
+                tree[index] = newVal;
+                added = true;
+            }
+            if (tree[index] == newVal) {
+                added = true;
+            }
+            else if (tree[index] > newVal) {
+                if (index == 0) {
+                    index = 1;
                 }
                 else {
-                    root = -1;
+                    index *= 2;
                 }
             }
+            else {
+                if (index == 0) {
+                    index = 2;
+                }
+                else {
+                    index = (index * 2) + 1;
+                }
+            }
+                                         
         }
-        else {
-            System.out.println("Tree is full.");
+    }
+   
+
+    public void preOrderTrav() {
+        preOrderTravH(0);
+    }
+
+    private void preOrderTravH(int index) {
+        if (index < tree.length && tree[index] != -1) {
+            if (index == 0) {
+                System.out.println(tree[index]);
+                preOrderTravH(1);
+                preOrderTravH(2);
+            }
+            else {
+                System.out.println(tree[index]);
+                preOrderTravH(index * 2);
+                preOrderTravH((index * 2) + 1);
+            }
         }
     }
 
-    public void preOrderTrav() {
+    public void inOrderTrav() {
+        inOrderTravH(0);
+    }
+
+    private void inOrderTravH(int index) {
+        if (index < tree.length && tree[index] != -1) {
+            if (index == 0) {
+                inOrderTravH(1);
+                System.out.println(tree[index]);
+                inOrderTravH(2);
+            }
+            else {
+                inOrderTravH(index * 2);
+                System.out.println(tree[index]);
+                inOrderTravH((index * 2) + 1);
+            }
+        }
+    }
+
+    public void postOrderTrav() {
+        postOrderTravH(0);
+    }
+
+    private void postOrderTravH(int index) {
+        if (index < tree.length && tree[index] != -1) {
+            if (index == 0) {
+                postOrderTravH(1);
+                postOrderTravH(2);
+                System.out.println(tree[index]);
+
+            }
+            else {
+            postOrderTravH(index * 2);
+            postOrderTravH((index * 2) + 1);
+            System.out.println(tree[index]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         
+        ArrayBST arbol = new ArrayBST(6);
+
+        arbol.insert( 4 );
+        arbol.insert( 2 );
+        arbol.insert( 5 );
+        arbol.insert( 6 );
+        arbol.insert( 1 );
+        arbol.insert( 3 );
+
+        System.out.println( "\npre-order traversal:" );
+        arbol.preOrderTrav();
+
+        System.out.println( "\nin-order traversal:" );
+        arbol.inOrderTrav();
+	
+        System.out.println( "\npost-order traversal:" );
+        arbol.postOrderTrav();	
+        /*~~~~~~~~~~~~move~me~down~~~~~~~~~~~~~~~~~~~~~~
+          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
     }
 
 }
